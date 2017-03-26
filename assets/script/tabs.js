4f4e999be790ad363a09b4c3fc5a2e0582576590
amanda107 <manda.107@hotmail.com>
2017-03-26 20:51:55 +0100

    seating added

--- assets/script/tabs.js
+++ assets/script/tabs.js
@@ -1,5 +1,5 @@
 // tabs
-function openCity(evt, cityName) {
+function tabSwitch(evt, tabName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -9,7 +9,7 @@ function openCity(evt, cityName) {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
 
