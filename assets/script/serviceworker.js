ffd1cd9f0a624556c34a90fd499b5841312e79f7
amanda107 <manda.107@hotmail.com>
2017-04-07 11:36:18 +0100

    cashe patch

--- assets/script/serviceworker.js
+++ assets/script/serviceworker.js
@@ -17,22 +17,8 @@ var CACHED_URLS = [
 ];
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(CACHED_URLS);
-    })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request).then(function(response) {
-        if (response) {
-          return response;
-        } else if (event.request.headers.get('accept').includes('text/html')) {
-          return caches.match('offline.html');
-        }
-      });
+    caches.open('gih-cache').then(function(cache) {
+      return cache.add('offline.html');
     })
   );
 });
