b4081f345483ebcf8d93221fdb7ac96a25c4399c
amanda107 <manda.107@hotmail.com>
2017-04-07 11:49:28 +0100

    sw build continued

--- assets/script/serviceworker.js
+++ assets/script/serviceworker.js
@@ -15,18 +15,26 @@ var CACHED_URLS = [
     'assets/img/news.svg',
     'assets/img/prom.svg'
 ];
+
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('gih-cache').then(function(cache) {
-      return cache.add('offline.html');
-    })
-  );
-});
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match('offline.html');
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHED_URLS);
     })
   );
 });
 
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get('accept').includes('text/html')) {
+          return caches.match('offline.html');
+        }
+      });
+    })
+  );
+});
