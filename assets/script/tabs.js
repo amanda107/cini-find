4957a994fe8fa2390e81b0cabc3f4c4eecea49b5
amanda107 <manda.107@hotmail.com>
2017-04-09 14:56:10 +0100

    alt tags added button animations added

--- assets/script/tabs.js
+++ assets/script/tabs.js
@@ -19,6 +19,22 @@ document.getElementById("defaultOpen").click();
 
 
 
+//buttons
+var button = document.querySelectorAll('.btn');
+for (var i = 0; i < button.length; i++) {
+  button[i].onmousedown = function(e) {
 
+    var x = (e.offsetX == undefined) ? e.layerX : e.offsetX;
+    var y = (e.offsetY == undefined) ? e.layerY : e.offsetY;
+    var effect = document.createElement('div');
+    effect.className = 'effect';
+    effect.style.top = y + 'px';
+    effect.style.left = x + 'px';
+    e.srcElement.appendChild(effect);
+    setTimeout(function() {
+      e.srcElement.removeChild(effect);
+    }, 1100);
+  }
+}
 
 
